5e4fc2aafb7bec77882f6f64530bd128735aeea2
zzqlplq <zzqlplq@gmail.com>
2020-08-21 20:04:56 +0800

    update

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
     targets: [
         .target(
             name: "PopupController",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
     ]
 )
